279d1ec1f03bd60692ae35fc28650649f2f4e9a1
JonaPlaz <jplazanet@hotmail.com>
2021-12-27 15:48:57 +0100

    feat(upgrade) : wip

--- symfony/src/Controller/Front/HomeController.php
+++ symfony/src/Controller/Front/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Front;
 
 use App\Form\ContactType;
+use App\Repository\CategoryRepository;
 use App\Repository\TvShowRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,36 +12,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * HomePage + 3 lastest TvShows or Movies
-     * 
-     * @Route("/", name="home")
-     */
-    public function home(TvShowRepository $tvShowRepository): Response
-    {
-        $latestTvshows = $tvShowRepository->findBy([], ['id' => 'DESC'], 3);
+  /**
+   * HomePage + 3 lastest TvShows or Movies
+   * 
+   * @Route("/", name="home")
+   */
+  public function home(TvShowRepository $tvShowRepository, CategoryRepository $categoryRepository): Response
+  {
+    $latestTvshows = $tvShowRepository->findBy([], ['id' => 'DESC'], 3);
+    $categories = $categoryRepository->findAll();
 
-        return $this->render('front/home/home.html.twig', [
-            'latestTvshows' => $latestTvshows,
-        ]);
+    return $this->render('front/home/home.html.twig', [
+      'latestTvshows' => $latestTvshows,
+      'categories' => $categories,
+    ]);
+  }
+
+  /**
+   * @Route("/contact", name="contact")
+   */
+  public function contact(Request $request)
+  {
+    $form = $this->createForm(ContactType::class);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      //  créer un service d'email ?
+      dd($form->getData());
     }
 
-    /**
-     * @Route("/contact", name="contact")
-     */
-    public function contact(Request $request)
-    {   
-        $form = $this->createForm(ContactType::class);
-        
-        $form->handleRequest($request);
-       
-        if ($form->isSubmitted() && $form->isValid()) {
-          //  créer un service d'email ?
-            dd($form->getData());
-        }
-
-        return $this->render('front/home/contact.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
+    return $this->render('front/home/contact.html.twig', [
+      'form' => $form->createView(),
+    ]);
+  }
 }
