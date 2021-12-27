476e0f4ea8b3a1c0587e641bdec8eb6e3edff0ab
JonaPlaz <jplazanet@hotmail.com>
2021-12-27 13:51:29 +0100

    feat(controllers) : SearchController and templates created wip

---
+++ symfony/src/Controller/Front/SearchController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\TvShowRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+  /**
+   * 
+   * Display search result
+   * 
+   * @Route("/search", name="search_results")
+   */
+  public function results(Request $request, TvShowRepository $tvShowRepository): Response
+  {
+    $searchTerm = $request->get('search');
+
+    $results = $tvShowRepository->findAllBySearchTermDQL($searchTerm);
+
+    return $this->render('front/search/results.html.twig', [
+      'results' => $results,
+      'searchTerm' => $searchTerm
+    ]);
+  }
+}
