ce56ff9e17eb477ce2deb573751e5d7224d8c801
JonaPlaz <jplazanet@hotmail.com>
2022-01-01 20:30:58 +0100

    feat(slugger) : slugger created - new fixtures wip

--- symfony/src/Controller/Front/TvShowController.php
+++ symfony/src/Controller/Front/TvShowController.php
@@ -4,7 +4,7 @@ namespace App\Controller\Front;
 
 use App\Entity\TvShow;
 use App\Repository\TvShowRepository;
-// use App\Service\Slugger;
+use App\Service\Slugger;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,55 +15,51 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class TvShowController extends AbstractController
 {
-    /**
-     * Display all TvShows
-     * 
-     * @Route("", name="browse")
-     */
-    public function browse(TvShowRepository $tvShowRepository): Response
-    {
-        return $this->render('front/tv_show/browse.html.twig', [
-            'tvShows' => $tvShowRepository->findAll(),
-        ]);
+  /**
+   * Display all TvShows
+   * 
+   * @Route("", name="browse")
+   */
+  public function browse(TvShowRepository $tvShowRepository): Response
+  {
+    return $this->render('front/tv_show/browse.html.twig', [
+      'tvShows' => $tvShowRepository->findAll(),
+    ]);
+  }
+
+  /**
+   * Display TvShow Details
+   * 
+   * @Route("/{id}", name="read", requirements={"id"="\d+"})
+   *
+   * @return Response
+   */
+  public function read(Slugger $slugger, EntityManagerInterface $manager, TvShow $tvShow): Response
+  {
+    if ($tvShow->getSlug() === null) {
+      $slugger->slugifyTvShow($tvShow);
+      $manager->flush();
     }
 
-    /**
-     * Display TvShow Details
-     * 
-     * @Route("/{id}", name="read", requirements={"id"="\d+"})
-     *
-     * @return Response
-     */
-    public function read(EntityManagerInterface $manager, TvShow $tvShow): Response
-    {
-        // Maintenant qu'on a le Slugger, on souhaite calculer le slug de la série,
-        // le mettre à jour en BDD et rediriger vers la page dont l'url s'écrit avec le slug (tvshow_read_slug)
+    // On redirige sur la route /tvshow/{slug}
+    return $this->redirectToRoute('tvshow_read_slug', [
+      'slug' => $tvShow->getSlug(),
+    ]);
 
-        // // Si le slug n'existait pas encore, on le calcule
-        // if ($tvShow->getSlug() === null) {
-        //     $slugger->slugifyTvShow($tvShow);
-        //     $manager->flush();
-        // }
+    return $this->render('front/tv_show/read.html.twig', [
+      'tvShow' => $tvShow,
+    ]);
+  }
 
-        // // On redirige sur la route /tvshow/{slug}
-        // return $this->redirectToRoute('tvshow_read_slug', [
-        //     'slug' => $tvShow->getSlug(),
-        // ]);
-
-        return $this->render('front/tv_show/read.html.twig', [
-          'tvShow' => $tvShow,
-      ]);
-    }
-
-    // /**
-    //  * @Route("/{slug}", name="read_slug")
-    //  *
-    //  * @return Response
-    //  */
-    // public function readSlug(TvShow $tvShow): Response
-    // {
-    //     return $this->render('front/tvshow/read.html.twig', [
-    //         'tvShow' => $tvShow
-    //     ]);
-    // }
+  /**
+   * @Route("/{slug}", name="read_slug")
+   *
+   * @return Response
+   */
+  public function readSlug(TvShow $tvShow): Response
+  {
+    return $this->render('front/tvshow/read.html.twig', [
+      'tvShow' => $tvShow
+    ]);
+  }
 }
